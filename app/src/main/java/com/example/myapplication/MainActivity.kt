73b2c7433362ab1614dec51a35c997b654512081
Gaurav <gsaraf895@gmail.com>
2021-06-25 18:04:01 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import  androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            startActivity(FlutterActivity.createDefaultIntent(this))
+
+        }
+
+
+
+    }
+}
