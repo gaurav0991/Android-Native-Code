a007bfcfcd32d3c64d05e2a32b88c408606cb543
Gaurav <gsaraf895@gmail.com>
2021-06-27 18:22:27 +0530

    Data exchange

---
+++ app/src/main/java/com/example/myapplication/FlutterViewActivity.kt
@@ -0,0 +1,70 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.FrameLayout
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterView
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
+import org.json.JSONObject
+
+
+class FlutterViewActivity : AppCompatActivity() {
+
+    companion object {
+        const val CHANNEL = "com.test.mydata/data"
+
+        fun startActivity(context: MainActivity) {
+            val intent = Intent(context, FlutterViewActivity::class.java)
+
+            context.startActivityForResult(intent, 100)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val flutterEngine = FlutterEngine(this)
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+        FlutterEngineCache.getInstance().put("id",flutterEngine);
+        flutterEngine.getNavigationChannel().setInitialRoute("/");
+        MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler { call, result ->
+            // manage method calls here
+            print(call.method.toString())
+            if (call.method == "FromClientToHost") {
+                val resultStr = call.arguments.toString()
+                print(resultStr)
+                val resultJson = JSONObject(resultStr)
+                val res = resultJson.getString("data")
+                print("YES")
+                Log.d("TAG","YES")
+                Log.d("TAG", res)
+                val intent = Intent()
+                intent.putExtra("result", res)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+
+
+            } else {
+                print("NO-")
+                finish()
+            }
+        }
+        Handler().postDelayed({
+            MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).invokeMethod("fromHostToClient", "hello")
+        }, 500)
+        startActivity(FlutterActivity.withCachedEngine("id").build(this))
+
+
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,45 @@
 package com.example.myapplication
 
-import  androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            if (resultCode == Activity.RESULT_OK) {
+                val operation = data?.extras?.getString("data")
+                Log.d("TAG",operation.toString())
+
+            } else {
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            startActivity(FlutterActivity.createDefaultIntent(this))
+
+
+        button.setOnClickListener {
+            FlutterViewActivity.startActivity(this);
 
         }
-
-
-
     }
 }
